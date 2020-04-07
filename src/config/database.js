8d3c5c5f66a533fc487e291536ab9b4d15a76329
NeitanDev <natan.miranda02@gmail.com>
2020-04-06 21:02:07 -0300

    configurações de conexão feita

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports ={
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'root',
+    password: 'root',
+    database: 'db_help_a_family',
+    define:{
+        timestamps: true,
+        underscored: true,
+    }
+};
