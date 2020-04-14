7ccab3db41127d9ea0974d89304a117770bea65d
NeitanDev <natan.miranda02@gmail.com>
2020-04-14 00:54:47 -0300

    adicionei o id na rota de listagem de familias no mapa

--- src/controllers/MapController.js
+++ src/controllers/MapController.js
@@ -3,7 +3,7 @@ const connection = require('../database/index');
 
 module.exports = {
     async listFamilias(req, res) {
-        const familys = await connection.query("SELECT nome, sobrenome, qtd_membros,longitude, latitude,mensagem FROM familias, fam_localizacoes WHERE fam_localizacoes.id_familia= familias.id ORDER BY familias.id;",
+        const familys = await connection.query("SELECT familias.id, nome, sobrenome, qtd_membros,longitude, latitude,mensagem FROM familias, fam_localizacoes WHERE fam_localizacoes.id_familia= familias.id ORDER BY familias.id;",
             { type: connection.QueryTypes.SELECT });
 
         return res.json(familys);
