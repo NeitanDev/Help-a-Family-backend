5e076c3c3bf8b7a4da1155da4f5e4f656c1c97f8
NeitanDev <natan.miranda02@gmail.com>
2020-04-19 18:55:08 -0300

    back ouve http e websocket

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
 const http = require('http');
+const socketio = require('socket.io');
 
 require('./database/index');
 
 const app = express();
 const server = http.Server(app);
+const io = socketio(server);
 
 app.use(express.json());
 app.use(routes);
