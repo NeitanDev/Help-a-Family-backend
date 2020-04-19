d59148418a269598f1762a648bfd180f51c91935
NeitanDev <natan.miranda02@gmail.com>
2020-04-19 18:36:53 -0300

    server add

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
+const http = require('http');
 
 require('./database/index');
 
 const app = express();
+const server = http.Server(app);
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
