79e90ef849e08a5402bee621607c3f6b86e35063
NeitanDev <natan.miranda02@gmail.com>
2020-04-19 18:59:55 -0300

    io.onconnection colocado no projeto

--- src/server.js
+++ src/server.js
@@ -9,6 +9,10 @@ const app = express();
 const server = http.Server(app);
 const io = socketio(server);
 
+io.on('connection', socket =>{
+    console.log('usuario conectado', socket.id);
+});
+
 app.use(express.json());
 app.use(routes);
 
